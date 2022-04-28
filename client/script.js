d20d800981ff7dd98924e207f2b7f5cbc9428569
Andrew Petersen <fespetersen1990@gmail.com>
2022-04-28 16:34:27 -0600

    problem 11 solved

--- client/script.js
+++ client/script.js
@@ -178,19 +178,19 @@ repeatButton.addEventListener('click', repeatMyParam);
 
 // CODE HERE
 
-// function queryRequest () {
-//     axios.get(`${baseURL}/query-test/?Jeddy`)
-//         .then((res) =>{
-//             console.log(res.data);
-//         })
-//         .catch((err) =>{
-//             console.log(err);
-//         })
-// }
+function queryRequest () {
+    axios.get(`${baseURL}/query-test/?Jeddy`)
+        .then((res) =>{
+            console.log(res.data);
+        })
+        .catch((err) =>{
+            console.log(err);
+        })
+}
 
-// const queryButton = document.getElementById('query-button')
+const queryButton = document.getElementById('query-button')
 
-// queryButton.addEventListener('click', queryRequest);
+queryButton.addEventListener('click', queryRequest);
 
 ////////////////
 //INTERMEDIATE//
@@ -237,19 +237,21 @@ repeatButton.addEventListener('click', repeatMyParam);
 //it console logged "you sent an empty query"
 
 //Problem 10.2
-function queryRequest () {
-    axios.get(`${baseURL}/query-test/?Jeddy?alec`)
-        .then((res) =>{
-            console.log(res.data);
-        })
-        .catch((err) =>{
-            console.log(err);
-        })
-}
+// function queryRequest () {
+//     axios.get(`${baseURL}/query-test/?Jeddy?alec`)
+//         .then((res) =>{
+//             console.log(res.data);
+//         })
+//         .catch((err) =>{
+//             console.log(err);
+//         })
+// }
 
-const queryButton = document.getElementById('query-button')
+// const queryButton = document.getElementById('query-button')
 
-queryButton.addEventListener('click', queryRequest);
+// queryButton.addEventListener('click', queryRequest);
+
+//it combined the two querys into one.//
 
 
 ////////////
@@ -258,21 +260,58 @@ queryButton.addEventListener('click', queryRequest);
 
 //PROBLEM 11
 /*
-    You are going to add the ability to POST to the server. You'll need to create a small form and write a function that makes a post request. Then you'll attach that function to the submit event on the form. We'll be creating a list of foods. 
+    You are going to add the ability to POST to the server. You'll need to create a small 
+    form and write a function that makes a post request. Then you'll attach that function to 
+    the submit event on the form. We'll be creating a list of foods. 
 
-    In the index.html file inside of the client folder, create a form with one text input field and a button. The input field should have a placeholder that tells the user to enter a food. And the button should indicate that it will add food into a list. 
+    In the index.html file inside of the client folder, create a form with one text input 
+    field and a button. The input field should have a placeholder that tells the user to enter 
+    a food. And the button should indicate that it will add food into a list. 
 
+    
     In this file (script.js), create a function called createFood. 
     
-    Inside the function, select the input you just created in the HTML and save it to a variable called foodInput. 
+    Inside the function, select the input you just created in the HTML and save it to a 
+    variable called foodInput. 
     
-    Next, create an object called body inside the function. It should have one key-value pair. The key should be newFood (make sure to match the case and spelling exactly) and the value should be the value of the food input. 
-
-    Now make an axios post request to /food. Inside the parentheses where you passed the URL in, pass in body as the second argument. 
-
-    Use a .then to handle the promise returned from the axios call. Pass a callback function to the .then. Inside that callback, console log the res.data. 
-
-    Based on what we did earlier to display this type of data, write code that will display the response in your HTML document. 
+    Next, create an object called body inside the function. It should have one key-value pair. 
+    The key should be newFood (make sure to match the case and spelling exactly) and the value 
+    should be the value of the food input. 
+    
+    Now make an axios post request to /food. Inside the parentheses where you passed the URL in, 
+    pass in body as the second argument. 
+    
+    Use a .then to handle the promise returned from the axios call. Pass a callback function to 
+    the .then. Inside that callback, console log the res.data. 
+    
+    |---------------------------------------------------------------------------------------------|
+    Based on what we did earlier to display this type of data, write code that will display the 
+    response in your HTML document. 
 */
 
 // CODE HERE 
+
+const foodForm = document.querySelector('#food-form')
+const foodInput = document.querySelector('#food-input');
+const unOrderedList = document.querySelector('ul');
+
+function createFood (event) {
+    event.preventDefault();
+    
+    let body = {
+        newFood: foodInput.value
+    };
+
+    axios.post(`${baseURL}/food`, body)
+        .then((res) =>{
+
+            let listItems = document.createElement('li')
+            listItems.textContent = foodInput.value
+            unOrderedList.appendChild(listItems);
+        })
+        .catch((err) =>{
+            console.log(err);
+        })
+}
+
+foodForm.addEventListener('submit', createFood);
